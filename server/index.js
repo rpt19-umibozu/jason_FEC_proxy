a3a9829c8163871353902faafb3fb6970e1a6bf5
bettergrammer <wiesnerjjason@gmail.com>
2020-03-31 23:43:38 -0700

    Cleanup files

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const fullPath = '/Users/jasonjacob/Desktop/seniorProjects/rpt19-front-end-capstone/jason_FEC_proxy/public/index.html';
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use('/', express.static(__dirname + '/../public'));
+
+app.use('/:id', express.static(__dirname + '/../public'));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
