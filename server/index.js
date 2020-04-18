294f90099ab394a8deae2f85e18247a1023d98df
bettergrammer <wiesnerjjason@gmail.com>
2020-04-18 16:15:55 -0700

    Add webpack

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const fullPath = '/home/ubuntu/jason_FEC_proxy/public/';
+const fullPath = '/home/ubuntu/jason_FEC_proxy/public/index.html';
+const expressStaticGzip = require("express-static-gzip");
+const path = require('path');
 
+
+app.use('/', expressStaticGzip(path.join(__dirname + '/../public'), {
+  enableBrotli: true,
+  customCompressions: [{
+      encodingName: 'deflate',
+      fileExtension: 'zz'
+  }],
+  orderPreference: ['br']
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
